9e22eb68979d7877e7ce9b8484397662cd36c324
chaidarsaad <chaidarsaad55@gmail.com>
2025-01-08 23:26:22 +0700

    update: lang

--- app/Filament/Resources/ExpenseResource/Pages/ListExpenses.php
+++ app/Filament/Resources/ExpenseResource/Pages/ListExpenses.php
@@ -10,11 +10,17 @@ use Filament\Actions\Action;
 use Filament\Forms\Components\FileUpload;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ListExpenses extends ListRecords
 {
     protected static string $resource = ExpenseResource::class;
+
+    public function getTitle(): string | Htmlable
+    {
+        return (__('Expense'));
+    }
 
     protected function getHeaderActions(): array
     {
